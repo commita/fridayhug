3ca506cd9be3932c04e42cb06357f6b60a2eb6c2
Kristopher Murata <kris@commita.com>
2012-08-23 15:18:12 -0400

    Ok, you are know the dumbest person in the world.

--- config/s3.rb
+++ config/s3.rb
@@ -4,9 +4,11 @@ require_relative 'load_path'
 
 # Uses Heroku config variables if they are present
 if ENV['AWS_KEY_ID']
-	 s3_config['access_key_id'] = ENV['AWS_KEY_ID']
-	 s3_config['secret_access_key'] = ENV['AWS_SECRET']
-	 s3_config['bucket'] = ENV['AWS_S3_BUCKET']
+	 s3_config = { 
+			'access_key_id' => ENV['AWS_KEY_ID'],
+	 		'secret_access_key' => ENV['AWS_SECRET'],
+	 		'bucket' => ENV['AWS_S3_BUCKET']
+		}
 else
 	s3_config = YAML::load(File.open("#{project_path}/config/s3.yml"))
 	s3_config = ENV['RACK_ENV'] == 'production' ? s3_config['production'] : s3_config['development']
