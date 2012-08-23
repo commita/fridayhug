9720c58da4202baefbda5da217412d8164616c84
Kristopher Murata <kris@commita.com>
2012-08-23 00:48:08 -0400

    Messing with Mongoid config for Heroku

--- config/config.rb
+++ config/config.rb
@@ -2,8 +2,7 @@ configure :production do
 	# Uses Heroku config variables if they are present
 	if ENV['MONGOLAB_URL']
 		Mongoid.configure do |config|
-  	  uri =  URI.parse(ENV['MONGOLAB_URL'])
-  	  config.master = Mongo::Connection.from_uri(ENV['MONGOLAB_URL']).db(uri.path.gsub("/", ""))    
+  	  config.uri = ENV['MONGOLAB_URL']
   	end
 	else
   	Mongoid.load!("#{settings.root}/config/mongoid.yml", :production)
